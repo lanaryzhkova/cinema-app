ad6ad3e33eeb4c373938bd555c08d0cc62ea1132
Lana Ryzhkova <lanalana2511@gmail.com>
2025-05-25 22:15:41 +0300

    feat: add action for movie (search)

--- src/cinema/movie.py
+++ src/cinema/movie.py
@@ -52,7 +52,7 @@ def input_year() -> int:
             print('Ошибка: нужно ввести число. Попробуйте снова.')
 
 
-def choose_from_list(options: list[any], prompt: str) -> None:
+def choose_from_list(options: list[any], prompt: str) -> str:
     print(prompt)
     for i, option in enumerate(options, start=1):
         name = option.value if hasattr(option, 'value') else str(option)
@@ -74,3 +74,34 @@ def show_list_all_movies() -> None:
         print('Список фильмов:')
         for i, movie in enumerate(movies.values(), start=1):
             print(f'{i}. {movie}')
+
+
+def search_movies():
+    if not movies:
+        print("Список фильмов пуст.")
+        return
+
+    print('### Поиск фильма по критериям ###')
+    parameters = ["Название", "Год производства", "Страна", "Жанр"]
+    result = []
+    choice = choose_from_list(parameters, "Выберите параметр для поиска:")
+
+    if choice == 'Название':
+        query = input("Введите название: ").lower()
+        result = [m for m in movies.values() if query in m.title.lower()]
+    elif choice == 'Год производства':
+        query = int(input("Введите год: "))
+        result = [m for m in movies.values() if query == m.year]
+    elif choice == 'Страна':
+        query = input("Введите страну: ")
+        result = [m for m in movies.values() if query in m.country.lower()]
+    elif choice == 'Жанр':
+        query = input("Введите жанр: ")
+        result = [m for m in movies.values() if query in m.genre.lower()]
+    if result:
+        print("Найденные фильмы:")
+        for movie in result:
+            print(movie)
+    else:
+        print("Фильмы по заданному критерию не найдены.")
+
