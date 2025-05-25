7e3667c239ed0fc5ac58854feb66cb134a6349e9
Lana Ryzhkova <lanalana2511@gmail.com>
2025-05-25 22:59:14 +0300

    refactor: add file utils for common function

--- src/cinema/movie.py
+++ src/cinema/movie.py
@@ -49,20 +49,6 @@ def input_year() -> int:
             print('Ошибка: нужно ввести число. Попробуйте снова.')
 
 
-def choose_from_list(options: list[any], prompt: str) -> str:
-    print(prompt)
-    for i, option in enumerate(options, start=1):
-        name = option.value if hasattr(option, 'value') else str(option)
-        print(f"{i}. {name}")
-    while True:
-        choice = input("Введите номер: ")
-        if choice.isdigit():
-            index = int(choice) - 1
-            if 0 <= index < len(options):
-                return options[index]
-        print("Неверный ввод, попробуйте ещё раз.")
-
-
 def show_list_all_movies() -> None:
     if not movies:
         print("Список фильмов пуст.")
---
+++ src/cinema/utils.py
@@ -0,0 +1,12 @@
+def choose_from_list(options: list[any], prompt: str) -> str:
+    print(prompt)
+    for i, option in enumerate(options, start=1):
+        name = option.value if hasattr(option, 'value') else str(option)
+        print(f"{i}. {name}")
+    while True:
+        choice = input("Введите номер: ")
+        if choice.isdigit():
+            index = int(choice) - 1
+            if 0 <= index < len(options):
+                return options[index]
+        print("Неверный ввод, попробуйте ещё раз.")
